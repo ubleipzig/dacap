75cf8087b29d25e2f688c5e2a62e2468771a2961
Ulf Seltmann <seltmann@ub.uni-leipzig.de>
2017-08-30 15:32:36 +0200

    added cross-origin support

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import * as debugFactory from 'debug';
 import * as path from 'path';
 import * as cache from './cache';
 import * as bodyparser from 'body-parser';
-
+import * as cors from 'cors';
 const debug = debugFactory('dacap:server');
 
 export class Server {
 	private register: cache.Register;
 	private expressApp: express.Application;
+	private corsOptions: cors.CorsOptions;
 
 	constructor(private config: {
 		storagePath: string,
@@ -22,21 +23,39 @@ export class Server {
 		autosaveInterval: number,
 		registerName: string
 	}) {
-		this._init();
+
+		this.expressApp = express();
+
+		this.initRegister();
+		this.initMiddlewares();
+		this.initRoutes();
 	}
 
-	private _init() {
+	private initRegister() {
 		this.register = new cache.Register(this.config.storagePath, this.config.registerName);
 		this.register.restore();
 		setTimeout(this.register.save, this.config.autosaveInterval * 1000);
 
-		this.expressApp = express();
-		this.expressApp.get(/favicon.ico/, (req, res, next) => {
-			res.send();
-		});
+	}
+
+	private initMiddlewares() {
+		this.corsOptions = {
+			origin: (origin, callback) => {
+				callback(null, true);
+			},
+			optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+		}
+		// globally allow all origins
+		this.expressApp.use(cors(this.corsOptions))
 
 		this.expressApp.use(compression());
 		this.expressApp.use(bodyparser.json());
+	}
+
+	private initRoutes() {
+		this.expressApp.get(/favicon.ico/, (req, res, next) => {
+			res.send();
+		});
 
 		this.expressApp.get('/admin/api/config', (req, res, next) => {
 			return res.json(this.config);
